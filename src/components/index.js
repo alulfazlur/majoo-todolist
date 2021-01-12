3b3971a054dca21ab4bbef73205711d5ffbbce3f
Fazlur Rahman <fazlur0504@gmail.com>
2021-01-13 00:18:58 +0700

    export components in one place

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Menu } from "./Menu";
+export { default as Note } from "./Note";
+export { default as AddButton } from "./AddButton";
+export { default as ModalNote } from "./ModalNote";
